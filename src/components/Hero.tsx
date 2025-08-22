e18ce34064aa51325c7c968aea0acf9c049ff8b1
Choudhury Mehbub Alam <109849791+devleo10@users.noreply.github.com>
2025-08-22 16:36:05 +0530

    Update resume link in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -303,7 +303,7 @@ const Hero: React.FC = () => {
             <a href="mailto:mehbubwork@gmail.com" style={{ textDecoration: 'none' }}>
               <AnimatedButton label="Contact Me" icon="mail" />
             </a>
-            <a href="https://drive.google.com/file/d/1SKH91atYdnex3UbCxM-yjf-YcfK1q9mc/view?usp=drivesdk" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+            <a href="https://drive.google.com/file/d/1ywgeUO1lhIorD3OU_W2Tu7fVBy7GDdAw/view?usp=sharing" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
               <AnimatedButton label="Resume" icon="resume" />
             </a>
           </div>
